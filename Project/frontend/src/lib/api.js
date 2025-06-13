933d0209261331c040c53e757fcd7041e4b7cd7a
Rithun-C <rithunthanthu123@gmail.com>
2025-06-13 16:50:14 +0530

    Frontend autentication

---
+++ Project/frontend/src/lib/api.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export const ENDPOINT = {
+    login : "/auth/login",
+    teacherRegister: "/auth/teacher/register",
+    studentRegister: "/auth/student/register"
+}
+
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
+export const api = axios.create({
+    baseURL: API_BASE_URL,
+    // credentials
+    withCredentials: true,
+});
